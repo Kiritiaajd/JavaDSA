8b90ce333858f0decfa229b7cada6469b8e8db43
KIRITI AAJAD <112309709+Kiritiaajd@users.noreply.github.com>
2024-07-13 02:59:23 +0530

    update

--- HashSetDemo01.java
+++ HashSetDemo01.java
@@ -1,5 +1,22 @@
+
+import java.util.*;
+
+@SuppressWarnings("unused")
 public class HashSetDemo01 {
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
-        
+        // HashSet<Integer> hs = new HashSet<>();
+        // @SuppressWarnings("rawtypes")
+        // HashSet<Integer> hs = new HashSet(100);
+        @SuppressWarnings("rawtypes")
+        HashSet hs = new HashSet<>();
+        hs.add(100);
+        hs.add("AMAN");
+        hs.add(90.09);
+        hs.add(true);
+        hs.add(null);
+        System.out.println(hs);
+        hs.remove(100);
+        System.out.println(hs);
     }
 }
