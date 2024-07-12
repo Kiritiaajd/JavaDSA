43da0e75a4f7a187f4ce77a5bf2e30f852f97035
KIRITI AAJAD <112309709+Kiritiaajd@users.noreply.github.com>
2024-07-13 03:12:11 +0530

    update

--- HashSetDemo01.java
+++ HashSetDemo01.java
@@ -18,5 +18,18 @@ public class HashSetDemo01 {
         System.out.println(hs);
         hs.remove(100);
         System.out.println(hs);
+        System.out.println("contained AMAN "+hs.contains("AMAN"));
+        System.out.println("isEmpty "+hs.isEmpty());
+        System.out.println("read usin for eah (not access by for loop) : ");
+        for (Object object : hs) {
+            System.out.print(""+object+" ");
+        }
+        System.out.println("using iterator ");
+        @SuppressWarnings("rawtypes")
+        Iterator it = hs.iterator();
+        while (it.hasNext()) {
+            System.out.print(it.next()+" ");
+        }
+
     }
 }
