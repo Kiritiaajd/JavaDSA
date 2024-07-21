47b7d84fb83890ff3d9f99a6191469e7089e49c2
KIRITI AAJAD <112309709+Kiritiaajd@users.noreply.github.com>
2024-07-22 05:04:24 +0530

    Recursion_002_Fibbonacci

--- Recursion_002_Fibbonacci.java
+++ Recursion_002_Fibbonacci.java
@@ -1,14 +1,18 @@
 public class Recursion_002_Fibbonacci {
     public static void main(String[] args) {
            System.out.println(fibbo(7));
-    }
+           System.out.println(10);
+    }          
     static int fibbo(int n){
 
         if (n<2) {
             return n;
             
         }
+        System.out.println(n);
        return fibbo(n-1) +fibbo(n-2);
         }
+
+        
     }
 
