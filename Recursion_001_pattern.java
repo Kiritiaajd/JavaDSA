e5d449ba87e4b4315fd2090f1aed2a133a2c8fde
KIRITI AAJAD <112309709+Kiritiaajd@users.noreply.github.com>
2024-07-28 19:27:10 +0530

    new function

--- Recursion_001_pattern.java
+++ Recursion_001_pattern.java
@@ -1,23 +1,35 @@
 public class Recursion_001_pattern {
-    public static void main(String[] args) {
-        Draw(5, 0);
-        Draw(5, 5);
-    }
+   public static void main(String[] args) {
+      Draw(5, 0);
+      Draw2(5, 0);
+   }
 
-    static void Draw( int row , int  col){
-         if (row == 0) {
-            return;
-         }
-         if (row > col) {
-            System.out.print("* ");
-            Draw(row, col+1 );
-         }else{
-            System.out.println();
-            Draw(row-1, 0);
-         }
-    }
+   static void Draw(int row, int col) {
+      if (row == 0) {
+         return;
+      }
+      if (row > col) {
+         System.out.print("* ");
+         Draw(row, col + 1);
+      } else {
+         System.out.println();
+         Draw(row - 1, 0);
 
-    static void Draw2(int row , int col){
-       
-    }
+      }
+   }
+
+   static void Draw2(int row, int col) {
+      if (row == 0) {
+         return;
+      }
+      if (col < row) {
+         Draw2(row, col + 1);
+         System.out.print("* ");
+
+      } else {
+
+         Draw2(row - 1, 0);
+         System.out.println();
+      }
+   }
 }
