530a78babc5cf9711c303d835bac38459c1f4c8d
KIRITI AAJAD <112309709+Kiritiaajd@users.noreply.github.com>
2024-07-31 08:08:36 +0530

    Recursion_011String

---
+++ Recursion_011String.java
@@ -0,0 +1,21 @@
+public class Recursion_011String {
+    public static void main(String[] args) {
+           String str = "baaadfvb aaaaabffnn";
+           
+           System.out.println(Substring(str, "", 0));
+    }
+
+    static String Substring(String str, String ans, int index) {
+        if (index == str.length()) {
+            return ans;
+        }
+
+        if (str.charAt(index) != 'a') {
+            ans = ans + str.charAt(index);
+            return Substring(str, ans, index + 1);
+        } else {
+            return Substring(str, ans, index + 1);
+        }
+
+    }
+}
