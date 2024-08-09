26f4b3114907e69ef61459da217ab81461bd321c
KIRITI AAJAD <112309709+Kiritiaajd@users.noreply.github.com>
2024-08-09 11:19:39 +0530

    SkipApple

--- Recursion_011String.java
+++ Recursion_011String.java
@@ -1,18 +1,20 @@
 public class Recursion_011String {
     public static void main(String[] args) {
-           String str = "baaadfvbaaaaabffnn";
-           
-           System.out.println(Substring(str, "", 0));
+        String str = "badfvbappleabfn";
 
-           skip("", str);
+        System.out.println(Substring(str, "", 0));
 
-           System.out.println(skip1(str));
+        skip("", str);
+
+        System.out.println(skip1(str));
+
+        System.out.println(skipapple(str));
     }
 
     static String Substring(String str, String ans, int index) {
         if (index == str.length()) {
             return ans;
-        }    
+        }
 
         if (str.charAt(index) != 'a') {
             ans = ans + str.charAt(index);
@@ -23,32 +25,45 @@ public class Recursion_011String {
 
     }
 
-    static void skip(String p , String up){
-             if (up.isEmpty()) {
-                System.out.println(p);
-                return;
-             } 
-             
-             char ch = up.charAt(0);
-             if (ch=='a') {
-                skip(p, up.substring(1));
+    static void skip(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
 
-             }else{
-                skip(p+ch, up.substring(1));
-             }
+        char ch = up.charAt(0);
+        if (ch == 'a') {
+            skip(p, up.substring(1));
+
+        } else {
+            skip(p + ch, up.substring(1));
+        }
     }
 
-    static String skip1( String up){
+    static String skip1(String up) {
         if (up.isEmpty()) {
-           return "";
-        } 
-        
-        char ch = up.charAt(0);
-        if (ch=='a') {
-           return skip1( up.substring(1));
-
-        }else{
-          return ch + skip1(up.substring(1));
+            return "";
         }
-}
+
+        char ch = up.charAt(0);
+        if (ch == 'a') {
+            return skip1(up.substring(1));
+
+        } else {
+            return ch + skip1(up.substring(1));
+        }
+    }
+
+    static  String skipapple(String up){
+
+        if (up.isEmpty()) {
+            return "";
+        }
+            if (up.startsWith("apple")) {
+                return skipapple(up.substring(5));
+            }
+            else{
+                return  up.charAt(0) + skipapple(up.substring(1));
+            }
+    }
 }
